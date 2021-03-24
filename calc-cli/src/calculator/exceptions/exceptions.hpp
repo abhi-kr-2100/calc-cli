bb77286c2710a691d51eca50e4bc08369a4ef22f
Abhishek Kumar <abhi.kr.2100@gmail.com>
2021-03-24 10:38:52 +0530

    Add what() to exceptions

--- calc-cli/src/calculator/exceptions/exceptions.hpp
+++ calc-cli/src/calculator/exceptions/exceptions.hpp
@@ -4,16 +4,51 @@
 
 
 #include <stdexcept>
+#include <string>
 
 
-class Calc_cli_exception : std::exception {};
-class Unbalanced_parentheses : Calc_cli_exception {};
-class Unknown_token : Calc_cli_exception {};
-class Bad_literal : Calc_cli_exception {};
-class Unsupported_operand : Calc_cli_exception {};
-class Syntax_error{};
-class Redeclaration_of_variable{};
-class Variable_not_defined{};
+class Calc_cli_exception : public std::exception {
+public:
+	Calc_cli_exception(const std::string& what="")
+			:err{ what } {
+	}
+
+	virtual const char* what() const noexcept override {
+		return err.c_str();
+	}
+
+private:
+	std::string err;
+};
+
+class Unbalanced_parentheses : public Calc_cli_exception {
+	using Calc_cli_exception::Calc_cli_exception;
+};
+
+class Unknown_token : public Calc_cli_exception {
+	using Calc_cli_exception::Calc_cli_exception;
+};
+
+class Bad_literal : public Calc_cli_exception {
+	using Calc_cli_exception::Calc_cli_exception;
+};
+
+class Unsupported_operand : public Calc_cli_exception {
+	using Calc_cli_exception::Calc_cli_exception;
+};
+
+class Syntax_error : public Calc_cli_exception {
+	using Calc_cli_exception::Calc_cli_exception;
+};
+
+class Redeclaration_of_variable : public Calc_cli_exception {
+	using Calc_cli_exception::Calc_cli_exception;
+};
+
+class Variable_not_defined : public Calc_cli_exception {
+	using Calc_cli_exception::Calc_cli_exception;
+};
+
 
 
 #endif // !CACL_CLI_EXCEPTIONS_HPP
